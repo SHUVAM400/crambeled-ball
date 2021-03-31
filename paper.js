446f7b939e9682e78abe11835b061e8e1baafdca
SHUVAM400 <78081836+SHUVAM400@users.noreply.github.com>
2021-03-31 11:12:59 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,65 @@
+class paper
+{
+	constructor(x,y)
+	{
+		var option={
+            isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2,
+        }
+		this.image=loadImage("paper.png")
+		this.bottomBody=Bodies.circle(this.x, this.y, this.paperWidth, this.wallThickness, {isStatic:true})
+		this.leftWallBody=Bodies.circle(this.x-this.paperWidth/2, this.y-this.paperHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		
+		
+
+		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		
+		World.add(world, this.bottomBody)
+		World.add(world, this.leftWallBody)
+		World.add(world, this.rightWallBody);
+
+	}
+	display()
+	{
+			var posBottom=this.bottomBody.position;
+			var posLeft=this.leftWallBody.position;
+			var posRight=this.rightWallBody.position;
+
+			
+
+			push()
+			translate(posLeft.x, posLeft.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			angleMode(RADIANS)
+			fill(255)
+			rotate(this.angle)
+			//rect(0,0,this.wallThickness, this.dustbinHeight);
+			pop()
+
+			push()
+			translate(posRight.x, posRight.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			angleMode(RADIANS)
+			fill(255)
+			rotate(-1*this.angle)
+			//rect(0,0,this.wallThickness, this.dustbinHeight);
+			pop()
+
+			push()
+			translate(posBottom.x, posBottom.y+10);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			angleMode(RADIANS)
+			fill(255)
+			imageMode(CENTER);
+			image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
+			//rect(0,0,this.dustbinWidth, this.wallThickness);
+			pop()
+			
+	}
+
+}
